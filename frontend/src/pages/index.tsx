286219c5a228a9dfba9591f38ac42528c1464473
Junior Furini <jrfurini@gmail.com>
2025-06-03 11:26:43 -0300

    chore: (Frontend) New visual for home page.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,63 +28,38 @@ export default function Home() {
       >
         <main className={styles.main}>
           <Image
-            className={styles.logo}
-            src="/next.svg"
+            src="/logo-house-numbers.avif"
             alt="Next.js logo"
-            width={180}
-            height={38}
+            width={236}
+            height={72}
             priority
           />
           <ol>
             <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
+              <Link href="/create_snippets">Create a new snippet.</Link>
             </li>
-            <li>Save and see your changes instantly.</li>
+            <li>
+              <Link href="/snippet">Query a snippet by ID.</Link>
+            </li>
           </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
         </main>
         <footer className={styles.footer}>
           <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/juniorfurini/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               aria-hidden
-              src="/file.svg"
+              src="/junior-furini.jpg"
               alt="File icon"
-              width={16}
-              height={16}
+              width={32}
+              height={32}
             />
-            Learn
+            Junior Furini
           </a>
           <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/jrfurini/hn_test_2025"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -94,21 +70,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
+            Github&apos;s repository
           </a>
         </footer>
       </div>
